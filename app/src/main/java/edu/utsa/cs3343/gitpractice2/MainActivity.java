faf2bc221fac89e6fc7d75b5c344f5547eccd1dc
Chaz Ortiz <chaz.ortiz@gmail.com>
2023-12-01 11:48:56 -0600

    Initial commit

---
+++ app/src/main/java/edu/utsa/cs3343/gitpractice2/MainActivity.java
@@ -0,0 +1,24 @@
+package edu.utsa.cs3343.gitpractice2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// notes
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int whatever;
+
+        String stuff;
+
+
+
+
+    }
+}
---
+++ app/src/main/java/edu/utsa/cs3343/gitpractice2/model/Stuff.java
@@ -0,0 +1,2 @@
+package edu.utsa.cs3343.gitpractice2.model;public class Stuff {
+}
